4c20ce5e210e50a59ab489ac6f03561c181e3f7b
edonahue <erich.donahue@gmail.com>
2015-11-16 01:49:24 -0500

    Completed Chap1 exercises 2, 3.

--- code/chap01ex.py
+++ code/chap01ex.py
@@ -1,26 +1,23 @@
-"""This file contains code for use with "Think Stats",
-by Allen B. Downey, available from greenteapress.com
-
-Copyright 2014 Allen B. Downey
-License: GNU GPLv3 http://www.gnu.org/licenses/gpl.html
-"""
-
-from __future__ import print_function
-
+import thinkstats2
 import numpy as np
-import sys
+import pandas as pd
 
 import nsfg
-import thinkstats2
 
+def ReadFemResp(dct_file='2002FemResp.dct',
+		dat_file='2002FemResp.dat.gz'):
+     dct = thinkstats2.ReadStataDct(dct_file)
+     df = dct.ReadFixedWidth(dat_file, compression='gzip')
+     return df
 
-def main(script):
-    """Tests the functions in this module.
+resp = ReadFemResp()
+# print resp.shape
 
-    script: string script name
-    """
-    print('%s: All tests passed.' % script)
+print resp.pregnum.value_counts().sort_index()
+print 'Total: ' + str(sum(resp.pregnum))
 
-
-if __name__ == '__main__':
-    main(*sys.argv)
+preg = nsfg.ReadFemPreg()
+preg_resps = {}
+for c in preg.caseid:
+    preg_resps[c] = preg_resps.get(c, 0) + 1
+print 'Preg data pregnancies: '+ str(sum(preg_resps.values()))
